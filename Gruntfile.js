41efe25ab7508de3728c5f2e42fd85af98265baa
clement <maboiteaspam@wanadoo.fr>
2013-09-19 15:01:55 +0800

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+
+module.exports = function(grunt) {
+
+    var d = __dirname+"/vendors/phantomizer-uglifyjs";
+
+    var in_dir = d+"/demo/in/";
+    var out_dir = d+"/demo/out/";
+    var meta_dir = d+"/demo/out/";
+
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json')
+
+        ,"out_dir":out_dir
+        ,"meta_dir":meta_dir
+
+        //-
+        ,'phantomizer-uglifyjs': {
+            options: {
+            }
+            ,test: {
+                files: {
+                    '<%= out_dir %>/jquery.ios-slider-0.9.1-min.js': [in_dir+'/jquery.ios-slider-0.9.1.js']
+                },
+                options:{
+                    "meta": "<%= meta_dir %>/jquery.ios-slider-0.9.1.js.meta"
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('phantomizer-uglifyjs');
+
+    grunt.registerTask('default',
+        [
+            'phantomizer-uglifyjs:test'
+        ]);
+};
---
+++ tasks/build.js
@@ -0,0 +1,143 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+    grunt.registerMultiTask("phantomizer-uglifyjs", "", function () {
+
+        var uglify = require('../lib/uglify').init(grunt);
+
+        var ph_libutil = require("phantomizer-libutil");
+        var meta = ph_libutil.meta
+        var wd = process.cwd()
+        var meta_manager = new meta( wd )
+
+        var options = this.options()
+
+        if( meta_manager.is_fresh(options.meta) == false ){
+            var done = this.async();
+            var current_grunt_task = this.nameArgs
+            var current_grunt_opt = this.options()
+            var deps = []
+
+            // add grunt file to dependencies so that file are rebuild when this file changes
+            for( var ooo in this.files ){
+                for( var ttt in this.files[ooo].src ){
+                    deps.push(this.files[ooo].src[ttt])
+                }
+            }
+            if ( grunt.file.exists(process.cwd()+"/Gruntfile.js")) {
+                deps.push(process.cwd()+"/Gruntfile.js")
+            }
+            deps.push(__filename)
+
+            // Merge task-specific and/or target-specific options with these defaults.
+            var options = this.options({
+                banner: '',
+                compress: {
+                    warnings: false
+                },
+                mangle: {},
+                beautify: false,
+                report: false
+            });
+
+            // Process banner.
+            var banner = grunt.template.process(options.banner);
+            var mapNameGenerator, mappingURLGenerator;
+
+            // Iterate over all src-dest file pairs.
+            this.files.forEach(function(f) {
+                var src = f.src.filter(function(filepath) {
+                    // Warn on and remove invalid source files (if nonull was set).
+                    if (!grunt.file.exists(filepath)) {
+                        grunt.log.warn('Source file "' + filepath + '" not found.');
+                        return false;
+                    } else {
+                        return true;
+                    }
+                });
+
+                // function to get the name of the sourceMap
+                if (typeof options.sourceMap === "function") {
+                    mapNameGenerator = options.sourceMap;
+                }
+
+                // function to get the sourceMappingURL
+                if (typeof options.sourceMappingURL === "function") {
+                    mappingURLGenerator = options.sourceMappingURL;
+                }
+
+                if (mapNameGenerator) {
+                    try {
+                        options.sourceMap = mapNameGenerator(f.dest);
+                    } catch (e) {
+                        var err = new Error('SourceMapName failed.');
+                        err.origError = e;
+                        grunt.fail.warn(err);
+                    }
+                }
+
+                if (mappingURLGenerator) {
+                    try {
+                        options.sourceMappingURL = mappingURLGenerator(f.dest);
+                    } catch (e) {
+                        var err = new Error('SourceMapName failed.');
+                        err.origError = e;
+                        grunt.fail.warn(err);
+                    }
+                }
+
+                // Minify files, warn and fail on error.
+                var result;
+                try {
+                    result = uglify.minify(src, f.dest, options);
+                } catch (e) {
+                    var err = new Error('Uglification failed.');
+                    if (e.msg) {
+                        err.message += ', ' + e.msg + '.';
+                    }
+                    err.origError = e;
+                    grunt.fail.warn(err);
+                }
+
+                // Concat banner + minified source.
+                var output = banner + result.min;
+
+                // Write the destination file.
+                grunt.file.write(f.dest, output);
+
+                // Write source map
+                if (options.sourceMap) {
+                    grunt.file.write(options.sourceMap, result.sourceMap);
+                    grunt.log.writeln('Source Map "' + options.sourceMap + '" created.');
+                }
+
+                // Print a success message.
+                grunt.log.writeln('File "' + f.dest + '" created.');
+
+                // ...and report some size information.
+                if (options.report) {
+                    contrib.minMaxInfo(result.min, result.max, options.report);
+                }
+            });
+
+
+            // create a cache entry, so that later we can regen or check freshness
+            var entry = meta_manager.create(deps)
+            entry.require_task(current_grunt_task, current_grunt_opt)
+
+            entry.save(options.meta, function(err){
+                if (err) done(false);
+                else{
+                    done();
+                }
+            })
+
+        }else{
+            console.log("your build is fresh !")
+        }
+
+
+    });
+
+};
